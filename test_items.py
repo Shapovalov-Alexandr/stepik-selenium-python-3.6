621ebe5f2d80a0fb87f395c38cfd3f987e5c2fa7
App-Ju <appju0.o@gmail.com>
2023-06-13 22:10:35 +0700

    delete time.sleep

--- test_items.py
+++ test_items.py
@@ -8,7 +8,6 @@ class TestCartButton:
     def test_presence_cart_button(self, browser):
         browser.implicitly_wait(6)
         browser.get(f"https://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
-        time.sleep(11)
 
         # bool(...) возвращает True либо Fals
         assert bool(browser.find_elements(By.CSS_SELECTOR, "button.btn-add-to-basket")), "Add to cart button missing"
