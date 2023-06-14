2a93713c5e303417efa8a139d46cb35cc95a1e5a
App-Ju <appju0.o@gmail.com>
2023-06-14 12:04:38 +0700

    fix test_items.py

--- test_items.py
+++ test_items.py
@@ -7,7 +7,7 @@ class TestCartButton:
 
     def test_presence_cart_button(self, browser):
         browser.implicitly_wait(6)
-        browser.get(f"https://selenium1py.pythonanywhere.com/catalogue/coders-at-work_2107/")
+        browser.get(f"https://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
 
         # bool(...) возвращает True либо Fals
         assert bool(browser.find_elements(By.CSS_SELECTOR, "button.btn-add-to-basket")), "Add to cart button missing"
